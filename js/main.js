f6de5569d0df37bd0b781c85488afafbedcb6f9c
mehmetturkuzanmSquareSolutions <mehmetturkuzan@hotmail.com>
2026-02-07 09:21:27 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 
 // Handle contact form (static)
-document.getElementById("contactForm").addEventListener("submit", function (e) {
+document.getElementById("contactForm")?.addEventListener("submit", function (e) {
   e.preventDefault();
   console.log("Contact form submitted");
   alert("Thank you! Your message has been received.");
